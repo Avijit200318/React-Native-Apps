f77c551fcb864f0855316f5bf58f4995e50f6bc5
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-12-13 11:28:57 +0530

    updated folder structure

---
+++ mobile-app/app/_layout.tsx
@@ -0,0 +1,24 @@
+import { Tabs } from 'expo-router';
+// import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import {Ionicons} from "@expo/vector-icons"
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name='settings'
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({color}) => <Ionicons size={28} name='settings' color={color} />
+        }}
+      />
+    </Tabs>
+  );
+}
