ed3ff368371fbb88663da4e3ae2781e609c4b16f
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2025-12-13 19:57:55 +0530

    created tab bar for our app

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -1,26 +1,41 @@
 import { Tabs } from 'expo-router'
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { AntDesign, Feather, Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { View } from 'react-native';
 
 export class TabLayout extends Component {
   render() {
     return (
-      <Tabs>
+      <Tabs screenOptions={{
+        headerShown: true,
+        tabBarActiveTintColor: "#f49b33",
+        tabBarInactiveTintColor: "#ecedee",
+        tabBarStyle: {
+          backgroundColor: "#2b2b2b",
+          paddingBottom: 20,
+          paddingTop: 5,
+          height: 110
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          marginTop: 5
+        },
+      }}>
         <Tabs.Screen name='home' options={{
-            title: "Home",
-            
+          title: "Home",
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="home" color={color} />
         }} />
         <Tabs.Screen name='history' options={{
-            title: "History",
-
+          title: "History",
+          tabBarIcon: ({ color }) => <AntDesign name="history" size={24} color={color} />
         }} />
         <Tabs.Screen name='profile' options={{
-            title: "Profile",
-
+          title: "Profile",
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="account-circle-outline" size={28} color={color} />
         }} />
         <Tabs.Screen name='settings' options={{
-            title: "Settings",
-
+          title: "Settings",
+          tabBarIcon: ({ color }) => <Feather name="settings" size={24} color={color} />
         }} />
       </Tabs>
     )
